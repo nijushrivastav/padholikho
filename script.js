723e8369d71d3ae67b4819a722efcf57c636b6d4
NijuShrivastav <nijushrivastavaa@gmail.com>
2025-04-19 01:38:46 +0530

    comments

---
+++ script.js
@@ -0,0 +1,52 @@
+// for(let i=1;i<=20;i++){
+//     console.log(i);
+// }
+
+// let i=1;
+// while(i<=20){
+//     console.log(i);
+//     i++
+// }
+
+//  for(let i=100; i>=1; i--){
+//     console.log(i)
+//  }
+
+// for(let i=5; i<=200; i+=5){
+//     console.log(i)
+// }
+
+// for(let i=1; i<=50; i++){
+//     if(i%2==0){
+//         console.log(i+" is even");
+//     }
+//     else{
+//         console.log(i+" is odd");
+//     }
+//  }
+
+// const largestofthree = (a,b,c)=>{
+//     if (a>=b && a>=c){
+//         return a;
+//     }
+//     else if(b>=a && b>=c){
+//         return b;
+//     }
+//     else{
+//     return c;
+// }
+// }
+//  console.log(largestofthree(10,23,67))
+
+// 4*3*2*1
+// function factorial(a) {
+//     let fact = 1;
+//     for(let i=a; i>0; i--){
+//    fact = fact*i
+//     }
+//     return fact;
+// }
+// console.log(factorial(20))
+
+// fact = 1 ,4 ,12 , 24, 24 
+
